ce4158c83796f5908ba533c0c619ee2a15476635
TPW_GeonBBang <74173893+GeonBBang0109@users.noreply.github.com>
2024-08-12 10:16:22 +0900

    update v0.01 >> v0.02

--- static/js/default.js
+++ static/js/default.js
@@ -8,7 +8,9 @@
 // 이용자의 페이지 위치에 따른 네비게이션 속성 변경
 const currentPath = window.location.pathname;
 const navLinks = document.querySelectorAll('.header-contents__nav');
+const mobileNavLinks = document.querySelectorAll('.mobile-header-contents__nav-contents__nav-link');
 const navHomeLinks = document.querySelectorAll('.header-contents__nav[href="/"]')
+const mobileNavHomeLinks = document.querySelectorAll('.mobile-header-contents__nav-contents__nav-link[href="/"]')
 
 navLinks.forEach(link => {
     let linkHref = link.getAttribute('href');
@@ -19,13 +21,44 @@ navLinks.forEach(link => {
     }
 });
 
+mobileNavLinks.forEach(link => {
+    let linkHref = link.getAttribute('href');
+    
+    if (linkHref === currentPath || (linkHref + '.html') === currentPath) {
+        link.classList.add('mobile-nav-active');
+        link.removeAttribute('href');
+    }
+});
+
 // 이용자가 메인 페이지를 '/index'로 접속한 경우
 if (currentPath === '/index') {
     navHomeLinks.forEach(homeLink => {
         homeLink.classList.add('nav-active');
         homeLink.removeAttribute('href');
     });
+
+    mobileNavHomeLinks.forEach(homeLink => {
+        homeLink.classList.add('mobile-nav-active');
+        homeLink.removeAttribute('href');
+    });
 }
+
+// Mobile Header 네비게이션 레이어 표시
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileHeaderCheckbox = document.querySelector('.mobile-header-contents__nav-button-check');
+    const mobileNavLayer = document.querySelector('.mobile-header-contents__nav-layer');
+    const mobileNavContents = document.querySelector('.mobile-header-contents__nav-contents')
+
+    mobileHeaderCheckbox.addEventListener('change', function() {
+        if (this.checked) {
+            mobileNavLayer.style.height = '100vh';
+            mobileNavContents.style.opacity = '1';
+        } else {
+            mobileNavLayer.style.height = '0';
+            mobileNavContents.style.opacity = '0';
+        }
+    });
+});
 
 // Footer가 보일 때 Footer Contents에 애니메이션 작동
 const footerIntersectionCallback = (entries, observer) => {
